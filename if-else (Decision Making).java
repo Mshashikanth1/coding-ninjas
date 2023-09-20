45a72c1159880e36cfd30b7ca35ce5ab97e2cf23
shashikanth <mallepally.shashikanthh@yahoo.com>
2023-09-20 22:57:59 +0530

    Create if-else (Decision Making).java

---
+++ if-else (Decision Making).java
@@ -0,0 +1,10 @@
+public class Solution {
+    //if-else (Decision Making)
+    //https://www.codingninjas.com/studio/problems/if-else-decision-making_8357235?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf
+    public static String compareIfElse(int a, int b) {
+        // Write your code here
+        if(a>b) return "greater";
+        else if(a<b) return "smaller";
+        return  "equal";
+    }
+}
